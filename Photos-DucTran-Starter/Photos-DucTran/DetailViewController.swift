47b609933a2eda6a97319c360afb4b1387254729
Admin <ruslanpopesku95@gmail.com>
2017-04-22 19:15:04 +0300

    open detailVC

---
+++ Photos-DucTran-Starter/Photos-DucTran/DetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  DetailViewController.swift
+//  Photos-DucTran
+//
+//  Created by Admin on 22.04.17.
+//  Copyright © 2017 Developers Academy. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var categoryImageView: UIImageView!
+    var image: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        categoryImageView.image = self.image
+        navigationItem.title = "Photo"
+    }
+}
